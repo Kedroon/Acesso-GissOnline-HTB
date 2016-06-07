4017412d300153d032b0c639ce0278702ab345e5
Miguel Figueiredo Marques <miguelkedroon@gmail.com>
2016-06-07 05:34:42 -0400

    First

---
+++ Acesso GissOnline HTB/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Acesso_GissOnline_HTB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.acessar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // acessar
+            // 
+            this.acessar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.acessar.Location = new System.Drawing.Point(99, 172);
+            this.acessar.Name = "acessar";
+            this.acessar.Size = new System.Drawing.Size(175, 62);
+            this.acessar.TabIndex = 0;
+            this.acessar.Text = "Acessar";
+            this.acessar.UseVisualStyleBackColor = true;
+            this.acessar.Click += new System.EventHandler(this.acessar_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Janeiro",
+            "Fevereiro",
+            "Março",
+            "Abril",
+            "Maio",
+            "Junho",
+            "Julho",
+            "Agosto",
+            "Setembro",
+            "Outubro",
+            "Novembro",
+            "Dezembro"});
+            this.comboBox1.Location = new System.Drawing.Point(45, 99);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(116, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(209, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(93, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mês";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(236, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ano";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.acessar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(370, 287);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.acessar);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Acesso GissOnline HTB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button acessar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Acesso GissOnline HTB/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Acesso_GissOnline_HTB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string month = DateTime.Now.ToString("MM");
+            string year = DateTime.Now.Year.ToString();
+            comboBox1.SelectedIndex = int.Parse(month)-1;
+            textBox1.Text = year;
+            Environment.SetEnvironmentVariable("webdriver.chrome.driver", "chromedriver.exe");
+
+        }
+
+        private void acessar_Click(object sender, EventArgs e)
+        {
+            int i = 1 + comboBox1.SelectedIndex;
+            string month;
+            if (i<10)
+            {
+                month = "0" + i;
+            }
+            else
+            {
+                month = i.ToString();
+            }
+            string year = textBox1.Text;
+            Console.WriteLine(month);
+            Console.WriteLine(year);
+            ChromeDriver js = driver();
+            Cookie cookie1 = new Cookie("PID", "2524");
+            Cookie cookie2 = new Cookie("MOBI", "560801");
+            Cookie cookie3 = new Cookie("TIPO", "0");
+            Cookie cookie4 = new Cookie("SUBTIPO", "");
+            Cookie cookie5 = new Cookie("mes", month);
+            Cookie cookie6 = new Cookie("ano", year);
+
+            js.Navigate().GoToUrl("https://www3.gissonline.com.br/interna/default.cfms");
+            js.Manage().Cookies.AddCookie(cookie1);
+            js.Manage().Cookies.AddCookie(cookie2);
+            js.Manage().Cookies.AddCookie(cookie3);
+            js.Manage().Cookies.AddCookie(cookie4);
+            js.Manage().Cookies.AddCookie(cookie5);
+            js.Manage().Cookies.AddCookie(cookie6);
+
+            js.Navigate().GoToUrl("https://www3.gissonline.com.br/recebidas/listaNotas.cfm?modalidade=T");
+        }
+
+        public ChromeDriver driver() {
+
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+            return new ChromeDriver(chromeDriverService, new ChromeOptions());
+
+        }
+    }
+}
